215fda9071bb9a9bcae95547a91adb438988ea9b
REAZN <lewis@reazn.me>
2023-12-21 00:40:07 +0000

    index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+"use client";
+
+import { usePokedex } from "@/hooks/usePokedex";
+import { PokemonCard } from "@/components/pokemon-card";
+import { SelectedPokemon } from "@/components/selected-pokemon";
+import { useEffect, useState } from "react";
+import { useQueryState } from "next-usequerystate";
+import { SearchIcon } from "lucide-react";
+import { useInView } from "react-intersection-observer";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Drawer } from "vaul";
 
 export default function Home() {
+  const [selected, setSelected] = useQueryState("selected");
+  const [search, setSearch] = useQueryState("search");
+  const [desktop, setDesktop] = useState<boolean>(false);
+
+  const { ref, inView } = useInView({ rootMargin: "400px" });
+  const { data, isLoading, fetchNextPage, hasNextPage } = usePokedex({ search: search || "" });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      void fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage, inView]);
+
+  useEffect(() => {
+    const updateMedia = () => {
+      if (window.innerWidth > 900) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    updateMedia();
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <div className="relative flex center justify-between gap-10 mt-8 h-fit">
+        <section className="w-full">
+          <search className="flex items-center bg-foreground rounded-xl shadow-poke h-14 text-sm px-4">
+            <SearchIcon className="w-6 h-6 text-red-500" />
+            <input
+              className="bg-transparent h-full w-full pl-4 outline-0 font-medium text-lg"
+              value={search ?? ""}
+              placeholder="Search Pokémon"
+              onChange={(e) => setSearch(e.target.value)}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          </search>
+          <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 mt-8">
+            {isLoading ? (
+              Array(15)
+                .fill("")
+                .map((_, index) => <Skeleton key={index} className="rounded-3xl min-w-52 w-full h-64" />)
+            ) : (
+              <>
+                {data?.pages.map((group, index) =>
+                  group.results.map((pokemon, index) => (
+                    <Drawer.Root key={index} open={!desktop && undefined}>
+                      <Drawer.Trigger onClick={() => setSelected(pokemon.name)}>
+                        <PokemonCard pokemon={pokemon.name} />
+                      </Drawer.Trigger>
+                      <Drawer.Portal>
+                        <Drawer.Overlay className="fixed z-30 inset-0 bg-black/40" />
+                        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-40 flex h-full max-h-[95%]">
+                          <SelectedPokemon selected={selected ?? "bulbasaur"} className="w-full" handle />
+                        </Drawer.Content>
+                      </Drawer.Portal>
+                    </Drawer.Root>
+                  )),
+                )}
+              </>
+            )}
+          </div>
+          <button
+            onClick={() => fetchNextPage()}
+            ref={ref}
+            className="w-full bg-foreground shadow-poke rounded-lg py-2 my-4 font-bold hover:brightness-125"
+          >
+            Load more
+          </button>
+        </section>
+        {desktop && (
+          <section className="sticky top-8 h-full">
+            <SelectedPokemon selected={selected ?? "bulbasaur"} />
+          </section>
+        )}
       </div>
     </main>
-  )
+  );
 }
